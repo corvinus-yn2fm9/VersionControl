542836959f80076f22a3df5fc94da56e02df8cd8
Zsófi Zsigray <zsofi.zsigray@stud.uni-corvinus.hu>
2021-10-01 21:37:22 +0200

    Merge

--- UserMaintenance/UserMaintenance/Resource1.Designer.cs
+++ UserMaintenance/UserMaintenance/Resource1.Designer.cs
@@ -70,20 +70,29 @@ namespace UserMaintenance {
         }
         
         /// <summary>
-        ///   Looks up a localized string similar to Fájlba írás.
+        ///   Looks up a localized string similar to Törlés.
         /// </summary>
-        internal static string File {
+        internal static string Delete {
             get {
-                return ResourceManager.GetString("File", resourceCulture);
+                return ResourceManager.GetString("Delete", resourceCulture);
             }
         }
         
         /// <summary>
-        ///   Looks up a localized string similar to Teljes név.
+        ///   Looks up a localized string similar to Utónév.
         /// </summary>
-        internal static string FullName {
+        internal static string FirstName {
             get {
-                return ResourceManager.GetString("FullName", resourceCulture);
+                return ResourceManager.GetString("FirstName", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Vezetéknév.
+        /// </summary>
+        internal static string LastName {
+            get {
+                return ResourceManager.GetString("LastName", resourceCulture);
             }
         }
     }
