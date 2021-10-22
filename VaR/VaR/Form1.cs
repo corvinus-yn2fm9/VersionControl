3918e3f8cd867c855f10ce6487887c623fd8d093
Zsófi Zsigray <zsofi.zsigray@stud.uni-corvinus.hu>
2021-10-22 20:34:02 +0200

    Adatok betöltése

--- VaR/VaR/Form1.cs
+++ VaR/VaR/Form1.cs
@@ -10,11 +10,16 @@ using System.Windows.Forms;
 
 namespace VaR
 {
+    
     public partial class Form1 : Form
     {
+        PortfolioEntities context = new PortfolioEntities();
+        List<Tick> Ticks;
         public Form1()
         {
             InitializeComponent();
+            Ticks = context.Ticks.ToList();
+            dataGridView1.DataSource = Ticks;
         }
     }
 }
