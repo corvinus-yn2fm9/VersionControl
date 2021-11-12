f01f0b8ef9c17e6f78deb7ec7af46f9de3640abc
Zsófi Zsigray <zsofi.zsigray@stud.uni-corvinus.hu>
2021-11-12 19:26:07 +0100

    Absztrakció kezelése

--- christmas/christmas/Form1.cs
+++ christmas/christmas/Form1.cs
@@ -39,7 +39,7 @@ namespace christmas
             var maxPosition = 0;
             foreach (var ball in _balls)
             {
-                ball.MoveBall();
+                ball.MoveToy();
                 if (ball.Left > maxPosition)
                     maxPosition = ball.Left;
             }
